1cbf57bea845e98930a49dc6f84abd6e99c4d451
Kaleeva Alina <Kaleeva.fam@mail.ru>
2021-02-02 18:44:29 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import DopContainer from "./default module/containers/DopContainer";
+
+
+function App() {
+  return <DopContainer />;
+
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import UserForm from "./default module/components/UserForm";
+
+ReactDOM.render(
+    <App />,
+  document.getElementById("root")
+)
+
+ReactDOM.render(
+  <UserForm />,
+document.getElementById("rootn")
+)
